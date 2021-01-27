185323f65184a6059e8e1ecf0b981f5acef0c171
Matteo Nunez <matteo.nunez22@gmail.com>
2021-01-26 23:19:25 -0500

    Initial commit

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# shopify-backend-dev-challenge
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAmHe1wzQvhRdwtEdda68O_QzDrOpinypI",
+    authDomain: "test-project-1-283222.firebaseapp.com",
+    databaseURL: "https://test-project-1-283222.firebaseio.com",
+    projectId: "test-project-1-283222",
+    storageBucket: "test-project-1-283222.appspot.com",
+    messagingSenderId: "246906246079",
+    appId: "1:246906246079:web:be2e1785046b6665e7c70a",
+    measurementId: "G-XC20B4H5DW"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();  
